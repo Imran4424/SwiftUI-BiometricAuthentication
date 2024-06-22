26904d77eee95e26f82c628915ef4725980deba4
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-06-22 10:00:09 +0600

    project iniy

---
+++ BiometricAuthentication/BiometricAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricAuthenticationApp.swift
+//  BiometricAuthentication
+//
+//  Created by Shah Md Imran Hossain on 22/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BiometricAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiometricAuthentication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BiometricAuthentication
+//
+//  Created by Shah Md Imran Hossain on 22/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
