3d5f3b9fd50325477cb706a9889835cf1e28666e
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-07-03 22:49:23 +0600

    UI login

--- BiometricAuthentication/ContentView.swift
+++ BiometricAuthentication/ContentView.swift
@@ -7,15 +7,69 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Home: View {
+    @State private var userName = ""
+    @State private var password = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer(minLength: 0)
+            
+            Image(systemName: "lock.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 250)
+            
+            HStack {
+                VStack(alignment: .leading, spacing: 12) {
+                    Text("Login")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                    
+                    Text("Please sign in to continue")
+                        .foregroundStyle(.white.opacity(0.5))
+                }
+                
+                Spacer(minLength: 0)
+            }
+            .padding()
+            
+            HStack {
+                Image(systemName: "envelope")
+                    .font(.title2)
+                    .foregroundStyle(.white)
+                TextField("EMAIL", text: $userName)
+            }
+            .padding()
+            .background(Color.white.opacity(0.12))
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+            .padding(.horizontal)
+            
+            HStack {
+                Image(systemName: "key.horizontal")
+                    .font(.title2)
+                    .foregroundStyle(.white)
+                TextField("PASSWORD", text: $password)
+            }
+            .padding()
+            .background(Color.white.opacity(0.12))
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+            .padding(.horizontal)
+            
+            
+            Spacer(minLength: 0)
         }
-        .padding()
+        .background(Color.indigo.ignoresSafeArea(.all, edges: .all))
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            Home()
+                .toolbar(.hidden)
+        }
     }
 }
 
